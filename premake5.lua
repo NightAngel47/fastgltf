dca1d02722c066c8ab9a5d8f2105afa8949b243f
Steven Drovie <NightAngel47@users.noreply.github.com>
2025-03-26 21:32:38 -0500

    Update premake5.lua
    Signed-off-by: Steven Drovie <NightAngel47@users.noreply.github.com>

--- premake5.lua
+++ premake5.lua
@@ -1,36 +1,22 @@
-include "fastgltf"
-
 project "fastgltf"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
-    staticruntime "on"
-
+	staticruntime "on"
+  
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
+  
 	files
 	{
-		"fastgltf/**.h",
-    	"fastgltf/**.hpp",
-    	"fastgltf/**.cpp"
+		"include/fastgltf/**.hpp",
+		"include/fastgltf/**.cpp",
 	}
 
 	includedirs
 	{
-		"fastgltf",
-		"fastgltf/include"
+		"include"
 	}
-
-	defines
-	{
-	}
-
-	links
-	{
-		"fastgltf"
-	}
-
+  
 	filter "system:windows"
 		systemversion "latest"
 
